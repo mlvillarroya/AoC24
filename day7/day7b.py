cd9b934a15107204a8b00f1b1284d2f315c37b33
Mikel López <mlvillarroya@gmail.com>
2024-12-07 21:32:19 +0100

    day7!!!!!!!

---
+++ day7/day7b.py
@@ -0,0 +1,36 @@
+from itertools import product
+
+def operate(a, b, op):
+    if op == '+':
+        return a+b
+    elif op == '*':
+        return a*b
+    elif op == '||':
+        return int(str(a)+str(b))
+    else:
+        raise ValueError("Operador no valido")
+
+def main():
+    # Lee el contenido del archivo
+    with open('data.txt', 'r') as archivo:
+        content = archivo.read().split('\n')
+    content = [row.split() for row in content]
+    total_result = 0
+    for row in content:
+        result_to_check = int(row[0][:-1])
+        numbers_to_check = [int(number) for number in row[1:]]
+        for op_possibility in product(["+","*","||"], repeat=len(numbers_to_check)-1):
+            result = numbers_to_check[0]
+            match = False
+            for i in range(len(op_possibility)):
+                result = operate(result, numbers_to_check[i+1], op_possibility[i])
+            if result == result_to_check:
+                match = True
+            if match:
+                total_result += result_to_check
+                break
+
+    print(total_result)
+
+if __name__ == "__main__":
+    main()
